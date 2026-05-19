b3ec4848dc12fa8fa7b4427dd3800e48b2ea0483
ParkHamin <jqkrgkals123@gmail.com>
2026-05-19 16:35:48 +0900

    feat: auth_screen.dart 구현

---
+++ lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('로그인 페이지입니다.'));
+  }
+}
