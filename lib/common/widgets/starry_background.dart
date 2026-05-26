3b2d420a4253d5f9cc6c2f7518beae114d4eaff7
ParkHamin <jqkrgkals123@gmail.com>
2026-05-26 16:54:34 +0900

    feat: 빛나는 배경 이미지 구현

---
+++ lib/common/widgets/starry_background.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../../features/auth/widgets/star_painter.dart';
+
+class StarryBackground extends StatelessWidget {
+  final Widget child;
+
+  const StarryBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: RadialGradient(
+          center: Alignment.center,
+          radius: 1.0,
+          colors: [
+            Color(0xFF5522AC),
+            Color(0xFF1B0B33),
+          ],
+          stops: [0.0, 0.91],
+        ),
+      ),
+      child: Stack(
+        children: [
+          CustomPaint(painter: StarPainter(), size: Size.infinite),
+          child,
+        ],
+      ),
+    );
+  }
+}
