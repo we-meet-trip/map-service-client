934e4ae17edac5806b14669912f40e8b2d136d94
ParkHamin <jqkrgkals123@gmail.com>
2026-07-31 16:00:49 +0900

    refactor: app_router.dart import 수정 (foundation -> material)

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import '../../common/layout/main_layout.dart';
