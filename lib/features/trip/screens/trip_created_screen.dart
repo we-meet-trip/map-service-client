eaa181aac7d8d6d6c8622fd19e92bd9422df361c
Hamin <jqkrgkals123@gmail.com>
2026-07-31 19:03:10 +0900

    fix: 에러 해결

--- lib/features/trip/screens/trip_created_screen.dart
+++ lib/features/trip/screens/trip_created_screen.dart
@@ -66,6 +66,8 @@ class _TripCreatedScreenState extends State<TripCreatedScreen> {
       _totalDurationMinutes = 25;
     }
     // 로그인 후 복귀 시 자동으로 저장 바텀시트 열기
+      _days = _stops.map((s) => s.day).toSet().toList()..sort();                                                                                                    
+      _selectedDay = _days.isNotEmpty ? _days.first : 1; 
     if (TripRepository.instance.autoSaveOnNext && isAuthenticated.value) {
       TripRepository.instance.autoSaveOnNext = false;
       _shouldAutoSave = true;
